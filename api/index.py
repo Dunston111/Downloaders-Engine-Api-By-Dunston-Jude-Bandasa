921aca009a2ee960354fb4a4b5a5a19e077fe818
Dunston Jude Bandasa <138220528+Dunston111@users.noreply.github.com>
2026-02-08 19:44:55 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -11,25 +11,25 @@ def get_video():
     try:
         url = request.json.get('url')
         
-        # 1. Read the cookie file from your app folder
         current_dir = os.path.dirname(os.path.abspath(__file__))
         original_cookie_path = os.path.join(current_dir, 'cookies.txt')
-        
-        # 2. Copy it to /tmp (The only writable area in Vercel)
         temp_cookie_path = "/tmp/cookies.txt"
+        
+        # Read/Write to /tmp to bypass Vercel's read-only lock
         with open(original_cookie_path, 'r') as f:
             data = f.read()
         with open(temp_cookie_path, 'w') as f:
             f.write(data)
 
         ydl_opts = {
-            'format': '18', 
-            'cookiefile': temp_cookie_path, # Point to the writable copy
+            # 'best' finds the highest quality file that is ALREADY merged (no ffmpeg needed)
+            'format': 'best', 
+            'cookiefile': temp_cookie_path,
             'quiet': True,
             'no_check_certificate': True,
             'extractor_args': {
                 'youtube': {
-                    'player_client': ['ios'],
+                    'player_client': ['android', 'ios'],
                     'player_skip': ['configs', 'webpage']
                 }
             }
